ec0a7cbdc7b3e2f211dc2c9f3c2a66fd63782bf1
Andreia Cifoni <andreiacifoni@gmail.com>
2022-06-11 16:40:00 -0400

    initializing the backend for passport authentication in Register and Login

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const cors = require("cors");
+const passport = require("passport");
+const passportLocal = require("passport-local").Strategy;
+const cookieParser = require("cookie-parser");
+const bcrypt = require("bcryptjs");
+const expressSession = require("express-session");
+
+const app = express();
