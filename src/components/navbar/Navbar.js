2c22f0d97b78b29f04b9e8131396db7b715a41b2
Andreia Cifoni <andreiacifoni@gmail.com>
2022-07-06 22:08:36 -0400

    navbar style

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import NavMenu from "./navMenu";
 import { Link } from "react-router-dom";
-import { BeakerIcon, PlusCircleIcon } from "@heroicons/react/solid";
+import { MenuIcon, XIcon } from "@heroicons/react/solid";
 import ".././layout/styles.css";
 
 const Navbar = () => {
@@ -12,18 +12,14 @@ const Navbar = () => {
   };
 
   return (
-    <nav>
-      <h1>Hello Soap!</h1>
-      <div>
-        <i onClick={onNavIcon} className="nav-icon">
-          {clicked ? (
-            <BeakerIcon className="icon" />
-          ) : (
-            <PlusCircleIcon className="icon" />
-          )}
+    <nav className="nav-container">
+      <h1 className="nav-title">Hello Soap!</h1>
+      <div className="nav-icon">
+        <i onClick={onNavIcon}>
+          {clicked ? <XIcon className="icon" /> : <MenuIcon className="icon" />}
         </i>
       </div>
-      <ul className={clicked ? "nav-full" : "nav-cell"}>
+      <ul className={clicked ? "nav-list  nav-cell" : "nav-list"}>
         {NavMenu.map((item, index) => {
           return (
             <li key={index}>
--- src/components/navbar/navMenu.js
+++ src/components/navbar/navMenu.js
@@ -2,22 +2,22 @@ const NavMenu = [
   {
     title: "Home",
     url: "/",
-    cName: "nav-menu",
+    cName: "nav-item",
   },
   {
     title: "Contact us",
     url: "#",
-    cName: "nav-menu",
+    cName: "nav-item",
   },
   {
     title: "Login",
     url: "/Login",
-    cName: "nav-menu",
+    cName: "nav-item",
   },
   {
     title: "Register",
     url: "/Register",
-    cName: "nav-menu",
+    cName: "nav-item",
   },
 ];
 
