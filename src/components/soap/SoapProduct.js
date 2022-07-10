4f32eaa78e61ce77466533a6410b5c82d53607e8
Andreia Cifoni <andreiacifoni@gmail.com>
2022-07-10 15:42:42 -0400

    created SoapProduct

---
+++ src/components/soap/SoapProduct.js
@@ -0,0 +1,82 @@
+const SoapProducts = [
+  {
+    image:
+      "https://res.cloudinary.com/deiacifoni/image/upload/v1657481398/SoapStore/IMG_5322_crurpt.jpg",
+    title: "Garden Swirl",
+    description: "",
+    ingredients: "",
+    price: "",
+  },
+  {
+    image:
+      "https://res.cloudinary.com/deiacifoni/image/upload/v1657481395/SoapStore/rainbow_p7jwbb.jpg",
+    title: "Rainbow",
+    description: "",
+    ingredients: "",
+    price: "",
+  },
+  {
+    image:
+      "https://res.cloudinary.com/deiacifoni/image/upload/v1657481395/SoapStore/crystals_nzlb7p.jpg",
+    title: "Purple Cristal",
+    description: "",
+    ingredients: "",
+    price: "",
+  },
+  {
+    image:
+      "https://res.cloudinary.com/deiacifoni/image/upload/v1657481400/SoapStore/IMG_20210315_174239789_2_qv5rpg.jpg",
+    title: "Canadian Flag",
+    description: "",
+    ingredients: "",
+    price: "",
+  },
+  {
+    image:
+      "https://res.cloudinary.com/deiacifoni/image/upload/v1657481399/SoapStore/IMG_5318_cwj3o0.jpg",
+    title: "Orange-Mango Smoothie",
+    description: "",
+    ingredients: "",
+    price: "",
+  },
+  {
+    image:
+      "https://res.cloudinary.com/deiacifoni/image/upload/v1657481396/SoapStore/yellow_marble_pkkpfv.jpg",
+    title: "Passion Fruit Delicious",
+    description: "",
+    ingredients: "",
+    price: "",
+  },
+  {
+    image:
+      "https://res.cloudinary.com/deiacifoni/image/upload/v1657481399/SoapStore/IMG_5279_megl8i.jpg",
+    title: "Canadian Hockey Disk",
+    description: "",
+    ingredients: "",
+    price: "",
+  },
+  {
+    image:
+      "https://res.cloudinary.com/deiacifoni/image/upload/v1657481395/SoapStore/garden_of_hearts_akcst7.jpg",
+    title: "Gardens of Heart",
+    description: "",
+    ingredients: "",
+    price: "",
+  },
+  {
+    image:
+      "https://res.cloudinary.com/deiacifoni/image/upload/v1657481396/SoapStore/spheres_wymzq6.jpg",
+    title: "Soap Spheres",
+    description: "",
+    ingredients: "",
+    price: "",
+  },
+  {
+    image:
+      "https://res.cloudinary.com/deiacifoni/image/upload/v1657481397/SoapStore/IMG_5284_rx1ndl.jpg",
+    title: "Berries Swirl",
+    description: "",
+    ingredients: "",
+    price: "",
+  },
+];
